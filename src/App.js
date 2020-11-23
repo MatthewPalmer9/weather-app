56fe13a00be986bef0dc80d15b7f4f0596a0f7da
MacBookPro <matthewpalmer9@outlook.com>
2020-11-23 14:18:03 -0600

    applied a submit button and input field styling. Button now has animation on hover.

--- src/App.js
+++ src/App.js
@@ -9,10 +9,27 @@ export default class App extends Component {
       search: ""
     }
   }
+
   render() {
     return (
-      <div className="brand">
-        <span>MattPDev Weather App</span>
+      <div className="app-container">
+
+        {/* Brand Bar */}
+        <div className="brand">
+          <span>MattPDev Weather App</span>
+        </div>
+
+        {/* Input field */}
+        <div className="input-container">
+          <label>Type Your Location: </label>
+          <input type="text" />
+          <button> SUBMIT </button>
+        </div>
+
+        {/* Weather Report */}
+        <div id="report">
+
+        </div>
       </div>
     )
   }
